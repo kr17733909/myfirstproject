030b950d71682be5a432fb8fe3030fe883889c45
kr17733909 <85891492+kr17733909@users.noreply.github.com>
2021-07-13 22:05:07 +0800

    Update hello.py
    test3

--- python_test/hello.py
+++ python_test/hello.py
@@ -1,2 +1,17 @@
+#coding:utf-8
+
 for i in range(5):
 	print("hello,world")
+
+list_my=[3,1,2,5,4,9,7,3]
+dict_my={}
+
+def select_list():
+    for i in list_my:
+        if i not in dict_my.keys():
+            dict_my[i] = 1
+        else:
+            print(dict_my)
+            print("重复的数字是：",i)
+
+select_list()
